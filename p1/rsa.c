e6ea00e4c47fc6da4f60f618ea0d736bea70cb83
binod <btm28657@hotmail.com>
2016-09-20 17:13:18 -0400

    startes some hw stuff rsa.c

--- p1/rsa.c
+++ p1/rsa.c
@@ -16,154 +16,170 @@
 /* utility function for read/write mpz_t with streams: */
 int zToFile(FILE* f, mpz_t x)
 { 
-	size_t i,len = mpz_size(x)*sizeof(mp_limb_t);
-	unsigned char* buf = malloc(len);
-	/* force little endian-ness: */
-	for (i = 0; i < 8; i++) {
-		unsigned char b = (len >> 8*i) % 256;
-		fwrite(&b,1,1,f);
-	}
-	Z2BYTES(buf,len,x);
-	fwrite(buf,1,len,f);
-	/* kill copy in buffer, in case this was sensitive: */
-	memset(buf,0,len);
-	free(buf);
-	return 0;
+  size_t i,len = mpz_size(x)*sizeof(mp_limb_t);
+  unsigned char* buf = malloc(len);
+  /* force little endian-ness: */
+  for (i = 0; i < 8; i++) {
+    unsigned char b = (len >> 8*i) % 256;
+    fwrite(&b,1,1,f);
+  }
+  Z2BYTES(buf,len,x);
+  fwrite(buf,1,len,f);
+  /* kill copy in buffer, in case this was sensitive: */
+  memset(buf,0,len);
+  free(buf);
+  return 0;
 }
 int zFromFile(FILE* f, mpz_t x)
 {
-	size_t i,len=0;
-	/* force little endian-ness: */
-	for (i = 0; i < 8; i++) {
-		unsigned char b;
-		/* XXX error check this; return meaningful value. */
-		fread(&b,1,1,f);
-		len += (b << 8*i);
-	}
-	unsigned char* buf = malloc(len);
-	fread(buf,1,len,f);
-	BYTES2(x,buf,len);
-	/* kill copy in buffer, in case this was sensitive: */
-	memset(buf,0,len);
-	free(buf);
-	return 0;
+  size_t i,len=0;
+  /* force little endian-ness: */
+  for (i = 0; i < 8; i++) {
+    unsigned char b;
+    /* XXX error check this; return meaningful value. */
+    fread(&b,1,1,f);
+    len += (b << 8*i);
+  }
+  unsigned char* buf = malloc(len);
+  fread(buf,1,len,f);
+  BYTES2Z(x,buf,len);
+  /* kill copy in buffer, in case this was sensitive: */
+  memset(buf,0,len);
+  free(buf);
+  return 0;
 }
 
 int rsa_keyGen(size_t keyBits, RSA_KEY* K)
 {
-	rsa_initKey(K);
-	/* TODO: write this.  Use the prf to get random byte strings of
-	 * the right length, and then test for primality (see the ISPRIME
-	 * macro above).  Once you've found the primes, set up the other
-	 * pieces of the key ({en,de}crypting exponents, and n=pq). */
+  rsa_initKey(K);
+  /* TODO: write this.  Use the prf to get random byte strings of
+   * the right length, and then test for primality (see the ISPRIME
+   * macro above).  Once you've found the primes, set up the other
+   * pieces of the key ({en,de}crypting exponents, and n=pq). */
 
-	mp_limb_t *get_rand_string; // prf randBytes needs this
-	int prime_or_not = 0;
+  mp_limb_t *get_rand_string; // prf randBytes needs this
+  int prime_or_not = 0;
 
 
-	// set the value for K->p
+  // set the value for K->p
 
-	get_rand_string = mpz_limbs_write(K->p, keyBits); // pointer to value of k->p
-	while (true){
-	  randBytes(get_rand_string, keyBits); // generates random byte string
-	  prime_or_not =ISPRIME(K->p); // sheck prime
+  get_rand_string = mpz_limbs_write(K->p, keyBits); // pointer to value of k->p
+  while (1){
+    randBytes((unsigned char*)get_rand_string, keyBits); // generates random byte string
+    prime_or_not =ISPRIME(K->p); // sheck prime
 	  
-	  if (prime_or_not ==2 || prime_or_not ==1) { // if 2 prime if 1 probably or the other way around, but good enough for us
-	    mpz_limbs_finish(K->p, keyBits); // allocate the value permanently
-	    break;
-	  }
+    if (prime_or_not ==2 || prime_or_not ==1) { // if 2 prime if 1 probably or the other way around, but good enough for us
+      mpz_limbs_finish(K->p, keyBits); // allocate the value permanently
+      break;
+    }
+  }
 
-	 // set the random value for K->q
-	get_rand_string = mpz_limbs_write(K->q, keyBits); // pointer to value of k->p
-	  while (true){
-	  randBytes(get_rand_string, keyBits); // generates random byte string
-	  prime_or_not =ISPRIME(K->p); // sheck prime
+    // set the random value for K->q
+    get_rand_string = mpz_limbs_write(K->q, keyBits); // pointer to value of k->p
+    while (1){
+      randBytes((unsigned char*)get_rand_string, keyBits); // generates random byte string
+      prime_or_not =ISPRIME(K->p); // sheck prime
 	  
-	  if (prime_or_not ==2 || prime_or_not ==1) { // if 2 prime if 1 probably or the other way around, but good enough for us
-	    mpz_limbs_finish(K->q, keyBits); // allocate the value permanently
-	    break;
-	  }
-	}
-	return 0;
+      if (prime_or_not ==2 || prime_or_not ==1) { // if 2 prime if 1 probably or the other way around, but good enough for us
+	mpz_limbs_finish(K->q, keyBits); // allocate the value permanently
+	break;
+      }
+    }
+
+    // temporarily store values to K->n, K->e
+    mpz_sub_ui(K->n,K->p,(unsigned int)( 1));
+    mpz_sub_ui(K->e,K->q,(unsigned int)( 1));
+    
+    mpz_mul(K->n , K->n, K->e); // it is n =p*q;
+
+    while(1){
+      get_rand_string = mpz_limbs_write(K->e, mpz_size(K->n) ); // pointer to value of k->e, same size as n 
+
+      if (mpz_cmp(K->e,K->p)>0 && mpz_cmp(K->e,K->q)>0) break;
+    }
+  
+    
+    return 0;
 }
 
-size_t rsa_encrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		RSA_KEY* K)
-{
-	/* TODO: write this.  Use BYTES2Z to get integers, and then
-	 * Z2BYTES to write the output buffer. */
-	return 0; /* TODO: return should be # bytes written */
-}
-size_t rsa_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		RSA_KEY* K)
-{
-	/* TODO: write this.  See remarks above. */
-	return 0;
-}
 
-size_t rsa_numBytesN(RSA_KEY* K)
-{
-	return mpz_size(K->n) * sizeof(mp_limb_t);
-}
+  size_t rsa_encrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
+		     RSA_KEY* K)
+  {
+    /* TODO: write this.  Use BYTES2Z to get integers, and then
+     * Z2BYTES to write the output buffer. */
+    return 0; /* TODO: return should be # bytes written */
+  }
+  size_t rsa_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
+		     RSA_KEY* K)
+  {
+    /* TODO: write this.  See remarks above. */
+    return 0;
+  }
 
-int rsa_initKey(RSA_KEY* K)
-{
-	mpz_init(K->d); mpz_set_ui(K->d,0);
-	mpz_init(K->e); mpz_set_ui(K->e,0);
-	mpz_init(K->p); mpz_set_ui(K->p,0);
-	mpz_init(K->q); mpz_set_ui(K->q,0);
-	mpz_init(K->n); mpz_set_ui(K->n,0);
-	return 0;
-}
+  size_t rsa_numBytesN(RSA_KEY* K)
+  {
+    return mpz_size(K->n) * sizeof(mp_limb_t);
+  }
 
-int rsa_writePublic(FILE* f, RSA_KEY* K)
-{
-	/* only write n,e */
-	zToFile(f,K->n);
-	zToFile(f,K->e);
-	return 0;
-}
-int rsa_writePrivate(FILE* f, RSA_KEY* K)
-{
-	zToFile(f,K->n);
-	zToFile(f,K->e);
-	zToFile(f,K->p);
-	zToFile(f,K->q);
-	zToFile(f,K->d);
-	return 0;
-}
-int rsa_readPublic(FILE* f, RSA_KEY* K)
-{
-	rsa_initKey(K); /* will set all unused members to 0 */
-	zFromFile(f,K->n);
-	zFromFile(f,K->e);
-	return 0;
-}
-int rsa_readPrivate(FILE* f, RSA_KEY* K)
-{
-	rsa_initKey(K);
-	zFromFile(f,K->n);
-	zFromFile(f,K->e);
-	zFromFile(f,K->p);
-	zFromFile(f,K->q);
-	zFromFile(f,K->d);
-	return 0;
-}
-int rsa_shredKey(RSA_KEY* K)
-{
-	/* clear memory for key. */
-	mpz_t* L[5] = {&K->d,&K->e,&K->n,&K->p,&K->q};
-	size_t i;
-	for (i = 0; i < 5; i++) {
-		size_t nLimbs = mpz_size(*L[i]);
-		if (nLimbs) {
-			memset(mpz_limbs_write(*L[i],nLimbs),0,nLimbs*sizeof(mp_limb_t));
-			mpz_clear(*L[i]);
-		}
-	}
-	/* NOTE: a quick look at the gmp source reveals that the return of
-	 * mpz_limbs_write is only different than the existing limbs when
-	 * the number requested is larger than the allocation (which is
-	 * of course larger than mpz_size(X)) */
-	return 0;
-}
+  int rsa_initKey(RSA_KEY* K)
+  {
+    mpz_init(K->d); mpz_set_ui(K->d,0);
+    mpz_init(K->e); mpz_set_ui(K->e,0);
+    mpz_init(K->p); mpz_set_ui(K->p,0);
+    mpz_init(K->q); mpz_set_ui(K->q,0);
+    mpz_init(K->n); mpz_set_ui(K->n,0);
+    return 0;
+  }
+
+  int rsa_writePublic(FILE* f, RSA_KEY* K)
+  {
+    /* only write n,e */
+    zToFile(f,K->n);
+    zToFile(f,K->e);
+    return 0;
+  }
+  int rsa_writePrivate(FILE* f, RSA_KEY* K)
+  {
+    zToFile(f,K->n);
+    zToFile(f,K->e);
+    zToFile(f,K->p);
+    zToFile(f,K->q);
+    zToFile(f,K->d);
+    return 0;
+  }
+  int rsa_readPublic(FILE* f, RSA_KEY* K)
+  {
+    rsa_initKey(K); /* will set all unused members to 0 */
+    zFromFile(f,K->n);
+    zFromFile(f,K->e);
+    return 0;
+  }
+  int rsa_readPrivate(FILE* f, RSA_KEY* K)
+  {
+    rsa_initKey(K);
+    zFromFile(f,K->n);
+    zFromFile(f,K->e);
+    zFromFile(f,K->p);
+    zFromFile(f,K->q);
+    zFromFile(f,K->d);
+    return 0;
+  }
+  int rsa_shredKey(RSA_KEY* K)
+  {
+    /* clear memory for key. */
+    mpz_t* L[5] = {&K->d,&K->e,&K->n,&K->p,&K->q};
+    size_t i;
+    for (i = 0; i < 5; i++) {
+      size_t nLimbs = mpz_size(*L[i]);
+      if (nLimbs) {
+	memset(mpz_limbs_write(*L[i],nLimbs),0,nLimbs*sizeof(mp_limb_t));
+	mpz_clear(*L[i]);
+      }
+    }
+    /* NOTE: a quick look at the gmp source reveals that the return of
+     * mpz_limbs_write is only different than the existing limbs when
+     * the number requested is larger than the allocation (which is
+     * of course larger than mpz_size(X)) */
+    return 0;
+  }
